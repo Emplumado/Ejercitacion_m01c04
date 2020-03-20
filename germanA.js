5c578cd5387b0a9805c389cb3ef285bf0a22819b
gersilva96 <gersilva96@hotmail.com>
2020-03-20 19:49:32 -0300

    Primera subida de archivos por German

---
+++ germanA.js
@@ -0,0 +1,6 @@
+let bandera = true;
+if(bandera){
+    console.log("Este es el archivo A creado por German");
+}else{
+    console.log("También es el archivo A pero con bandera = false");
+}
---
+++ germanC.js
@@ -0,0 +1,7 @@
+let virus = false;
+
+if(virus){
+    console.log("Tenés coronavirus");
+}else{
+    console.log("No tenés coronavirus");
+}
